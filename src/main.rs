8b56f06ba736c5e47bc989a341397689d86d4ed6
Knarkzel <knarkzel@gmail.com>
2023-02-11 20:46:10 +0100

    Columns working

--- src/main.rs
+++ src/main.rs
@@ -4,49 +4,60 @@ fn main() {
     dioxus_web::launch(app);
 }
 
+#[derive(Props)]
+struct ProductProps<'a> {
+    name: &'a str,
+    price: usize,
+    image: &'a str,
+    fallback_image: &'a str,
+}
+
+fn Product<'a>(cx: Scope<'a, ProductProps<'a>>) -> Element {
+    cx.render(rsx! {
+        div {
+            class: "column is-one-third-desktop",
+            article {
+                class: "box",
+                p {
+                    class: "title",
+                    "{cx.props.name}",
+                },
+                p {
+                    class: "subtitle",
+                    "${cx.props.price} each",
+                },
+                picture {
+                    class: "image is-square",
+                    source {
+                        "type": "image/webp",
+                        "srcset": "{cx.props.image}",
+                    },
+                    img {
+                        src: "{cx.props.fallback_image}",
+                    },
+                },
+                button {
+                    class: "button title is-5 mt-4 is-fullwidth",
+                    "Add to cart",
+                },
+            }
+        },
+    })
+}
+
 fn app(cx: Scope) -> Element {
     cx.render(rsx! {
         main {
-            class: "container",
+            class: "container ",
             h1 {
-                class: "title my-4 is-1 has-text-centered",
+                class: "title is-1 has-text-centered mt-2",
                 "Shop",
             },
-            (0..3).map(|_| rsx! {
-                div {
-                    class: "tile is-ancestor",
-                    (0..3).map(|_| rsx! {
-                        div {
-                            class: "tile is-parent",
-                            article {
-                                class: "tile is-child box",
-                                p {
-                                    class: "title",
-                                    "Cat",
-                                },
-                                p {
-                                    class: "subtitle",
-                                    "$25 each",
-                                },
-                                picture {
-                                    class: "image is-square",
-                                    source {
-                                        "type": "image/webp",
-                                        "srcset": "images/cat.webp",
-                                    },
-                                    img {
-                                        src: "images/cat.jpg",
-                                    },
-                                },
-                                button {
-                                    class: "button title is-5 mt-4 is-fullwidth",
-                                    "Add to cart",
-                                },
-                            },
-                        },
-                    }),
-                },
-            }),
+            div {
+                class: "columns",
+                Product { name: "Cat", price: 25, image: "images/cat.webp", fallback_image: "images/cat.jpg" },
+                Product { name: "Dog", price: 50, image: "images/dog.webp", fallback_image: "images/dog.jpg" },
+            },
         },
     })
 }
