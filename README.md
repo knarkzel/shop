a0bfb12293aedf0a6499c560594287b870ad1724
Knarkzel <knarkzel@gmail.com>
2023-02-11 18:41:52 +0100

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# shop
+
+```
+git clone https://github.com/knarkzel/shop
+cd shop
+nix develop
+trunk serve
+```
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use dioxus::prelude::*;
+
+fn main() {
+    dioxus_web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! {
+        main {
+            class: "container",
+            h1 {
+                class: "title my-4 is-1 has-text-centered",
+                "Shop",
+            },
+            (0..3).map(|_| rsx! {
+                div {
+                    class: "tile is-ancestor",
+                    (0..3).map(|_| rsx! {
+                        div {
+                            class: "tile is-parent",
+                            article {
+                                class: "tile is-child box",
+                                p {
+                                    class: "title",
+                                    "Cat",
+                                },
+                                p {
+                                    class: "subtitle",
+                                    "$25 each",
+                                },
+                                figure {
+                                    class: "image is-square",
+                                    img {
+                                        src: "/images/cat.webp",
+                                    },
+                                },
+                                button {
+                                    class: "button title is-5 mt-4 is-fullwidth",
+                                    "Add to cart",
+                                },
+                            },
+                        },
+                    }),
+                },
+            }),
+        },
+    })
+}
